33972feb464047cd7a38665a4352bf76e29374b9
Hdulull <52431478+Hdulull@users.noreply.github.com>
2019-11-11 15:13:06 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './components/home';
+import Total from './components/Total';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/> 
+      <Total />
+    </div>
+  
+  
+  
+  );
+}
+
+export default App;
